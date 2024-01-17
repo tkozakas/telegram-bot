838595cc2289008962f9ffd8079d960fce74bf69
tomas6446 <tomas.kozakas@gmail.com>
2024-01-17 13:34:17 +0200

    Add yaml parameters

--- src/main/java/org/churk/telegrampibot/config/BotConfig.java
+++ src/main/java/org/churk/telegrampibot/config/BotConfig.java
@@ -1,14 +1,17 @@
 package org.churk.telegrampibot.config;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Data
 @Configuration
+@ConfigurationProperties("bot")
 public class BotConfig {
-    @Value("${bot.username}")
     private String username;
-    @Value("${bot.token}")
     private String token;
+    private String winnerName;
+    private List<String> stickerSets;
 }
