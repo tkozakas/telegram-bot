86bf08861124fd433ef00414cf571435e90303a2
tomas6446 <tomas.kozakas@gmail.com>
2024-01-18 14:20:38 +0200

    Refactor script

--- init-db.sql
+++ init-db.sql
@@ -7,8 +7,8 @@ CREATE TABLE IF NOT EXISTS stats
     user_id     BIGINT       NOT NULL,
     first_name  VARCHAR(255) NOT NULL,
     score       INTEGER      NOT NULL DEFAULT 0,
-    created_at  TIMESTAMP    NOT NULL DEFAULT NOW(),
-    is_winner   BOOLEAN      NOT NULL DEFAULT FALSE
+    is_winner   BOOLEAN      NOT NULL DEFAULT FALSE,
+    created_at  TIMESTAMP    NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS stickers
