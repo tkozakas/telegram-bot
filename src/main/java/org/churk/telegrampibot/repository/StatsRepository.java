b27bc9b45684b9f0787e2c034a237f435ffc474d
tomas6446 <tomas.kozakas@gmail.com>
2024-01-18 14:20:09 +0200

    Add queries

--- src/main/java/org/churk/telegrampibot/repository/StatsRepository.java
+++ src/main/java/org/churk/telegrampibot/repository/StatsRepository.java
@@ -2,7 +2,22 @@ package org.churk.telegrampibot.repository;
 
 import org.churk.telegrampibot.model.Stats;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface StatsRepository extends JpaRepository<Stats, Long> {
+    @Query("SELECT s FROM Stats s WHERE s.chatId = ?1 AND s.userId = ?2")
+    List<Stats> findStatsByChatIdAndUserId(Long chatId, Long userId);
 
+    @Query("SELECT s FROM Stats s WHERE s.chatId = ?1 AND YEAR(s.createdAt) = ?2")
+    List<Stats> findStatsByChatIdAndYear(Long chatId, int year);
+
+    @Query("SELECT s.isWinner FROM Stats s WHERE s.isWinner = TRUE")
+    boolean existsIsWinner();
+
+    @Query("SELECT s.userId FROM Stats s WHERE s.userId = ?1")
+    boolean existsByUserId(Long userId);
 }
