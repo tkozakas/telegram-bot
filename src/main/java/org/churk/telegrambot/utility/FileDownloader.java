a5d27a8773c437d7193e8f6490094a5d38f3ff26
tomas6446 <tomas.kozakas@gmail.com>
2024-01-19 17:14:55 +0200

    Add framework for file manipulation to compress files

--- src/main/java/org/churk/telegrambot/utility/FileDownloader.java
+++ src/main/java/org/churk/telegrambot/utility/FileDownloader.java
@@ -1,27 +1,25 @@
 package org.churk.telegrambot.utility;
 
+import com.github.kokorin.jaffree.ffmpeg.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import javax.imageio.ImageIO;
-import javax.imageio.ImageWriteParam;
-import javax.imageio.ImageWriter;
-import javax.imageio.stream.ImageOutputStream;
-import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.URL;
-import java.util.Iterator;
+import java.nio.file.Paths;
 
 @Slf4j
 @Component
 public class FileDownloader {
-    private static final int timeOutSeconds = 30;
+    private static final int TIME_OUT_SECONDS = 30;
+    private static final int WIDTH = 320;
+    private  static final int HEIGHT = 240;
 
     public static String waitForDownload(String downloadDirectory, String fileName, String extension) {
         String filePath = downloadDirectory + fileName + "_compressed" + extension;
         long startTime = System.currentTimeMillis();
         while (true) {
-            if (System.currentTimeMillis() - startTime > timeOutSeconds * 1000) {
+            if (System.currentTimeMillis() - startTime > TIME_OUT_SECONDS * 1000) {
                 log.error("Timeout while waiting for meme download");
                 return null;
             }
@@ -54,81 +52,50 @@ public class FileDownloader {
             }
 
             compressFile(extension, filePath, compressedFilePath);
+            File file = new File(filePath);
+            file.deleteOnExit();
         } catch (IOException e) {
             log.error("Error while downloading or compressing file", e);
         }
     }
 
-    private static void compressFile(String extension, String filePath, String compressedFilePath) throws IOException {
+    private static void compressFile(String extension, String filePath, String compressedFilePath) {
         if (extension.equals(".gif")) {
             compressGif(filePath, compressedFilePath);
         } else {
-            compressImage(extension, filePath, compressedFilePath);
+            compressImage(filePath, compressedFilePath);
         }
     }
 
-    private static void compressImage(String extension, String filePath, String compressedFilePath) {
+    private static void compressImage(String filePath, String compressedFilePath) {
         try {
-            // Read the downloaded image
-            BufferedImage image = ImageIO.read(new File(filePath));
-
-            // Compress the image
-            File compressedImageFile = new File(compressedFilePath);
-            OutputStream os = new FileOutputStream(compressedImageFile);
-
-            Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName(getValidFormat(extension));
-            if (!writers.hasNext()) {
-                throw new IOException("No suitable ImageWriter found for extension: " + extension);
-            }
-            ImageWriter writer = writers.next();
-
-            ImageOutputStream ios = ImageIO.createImageOutputStream(os);
-            writer.setOutput(ios);
-
-            ImageWriteParam param = writer.getDefaultWriteParam();
-            if (param.canWriteCompressed()) {
-                param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-                param.setCompressionQuality(0.05f);
-            }
-            writer.write(null, new javax.imageio.IIOImage(image, null, null), param);
-
-            os.close();
-            ios.close();
-            writer.dispose();
-
+            FFmpeg.atPath()
+                    .addInput(UrlInput.fromPath(Paths.get(filePath)))
+                    .addOutput(UrlOutput.toPath(Paths.get(compressedFilePath)))
+                    .setComplexFilter(FilterGraph.of(
+                            FilterChain.of(
+                                    Filter.withName("scale").addArgument(320 + ":" + 240)
+                            )
+                    ))
+                    .execute();
             log.info("File compressed and saved as: {}", compressedFilePath);
-        } catch (IOException e) {
-            log.error("Error while compressing file", e);
+        } catch (Exception e) {
+            log.error("Error compressing the image: {}", e.getMessage());
         }
     }
 
     private static void compressGif(String filePath, String compressedFilePath) {
-        try {
-            // Read the downloaded image
-            BufferedImage image = ImageIO.read(new File(filePath));
-
-            // Compress the image
-            File compressedImageFile = new File(compressedFilePath);
-            OutputStream os = new FileOutputStream(compressedImageFile);
-
-            ImageOutputStream ios = ImageIO.createImageOutputStream(os);
-            ImageIO.write(image, "gif", ios);
-
-            os.close();
-            ios.close();
-
-            log.info("File compressed and saved as: {}", compressedFilePath);
-        } catch (IOException e) {
-            log.error("Error while compressing file", e);
-        }
-    }
-
-    private static String getValidFormat(String extension) {
-        return switch (extension.toLowerCase()) {
-            case ".jpg", ".jpeg" -> "jpeg";
-            case ".png" -> "png";
-            case ".gif" -> "gif";
-            default -> extension;
-        };
+        FFmpeg.atPath()
+                .addInput(UrlInput.fromPath(Paths.get(filePath)))
+                .addOutput(UrlOutput.toPath(Paths.get(compressedFilePath)))
+                .setComplexFilter(FilterGraph.of(
+                        FilterChain.of(
+                                Filter.withName("fps").addArgument("fps=8"),
+                                Filter.withName("scale").addArgument(WIDTH + ":" + HEIGHT),
+                                Filter.withName("setpts").addArgument("4/10*PTS")
+                        )
+                ))
+                .execute();
+        log.info("File compressed and saved as: {}", compressedFilePath);
     }
 }
