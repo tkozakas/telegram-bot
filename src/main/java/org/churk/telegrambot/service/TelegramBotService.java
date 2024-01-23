704a7f8ae983e8ded5dde575ecc9079d62579787
tomas6446 <tomas.kozakas@gmail.com>
2024-01-23 18:35:54 +0200

    Refactor

--- src/main/java/org/churk/telegrambot/service/TelegramBotService.java
+++ src/main/java/org/churk/telegrambot/service/TelegramBotService.java
@@ -1,5 +1,7 @@
 package org.churk.telegrambot.service;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
@@ -24,15 +26,14 @@ import static java.lang.Thread.sleep;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class TelegramBotService extends TelegramLongPollingBot {
     private static final boolean ENABLED = true;
     private final BotProperties botProperties;
     private final MessageService messageService;
 
-    public TelegramBotService(BotProperties botProperties, MessageService messageService) throws TelegramApiException {
-        this.botProperties = botProperties;
-        this.messageService = messageService;
-
+    @PostConstruct
+    private void registerBotCommands() throws TelegramApiException {
         List<Pair<String, String>> commands = List.of(
                 Pair.of("/%sreg".formatted(botProperties.getWinnerName()), "Register yourself as a " + botProperties.getWinnerName()),
                 Pair.of("/%s".formatted(botProperties.getWinnerName()), "Get today's " + botProperties.getWinnerName()),
@@ -46,10 +47,6 @@ public class TelegramBotService extends TelegramLongPollingBot {
         List<BotCommand> botCommandList = commands.stream()
                 .map(command -> new BotCommand(command.getLeft(), command.getRight()))
                 .toList();
-        registerBotCommands(botCommandList);
-    }
-
-    private void registerBotCommands(List<BotCommand> botCommandList) throws TelegramApiException {
         this.execute(new SetMyCommands(botCommandList, new BotCommandScopeDefault(), null));
     }
 
@@ -81,9 +78,6 @@ public class TelegramBotService extends TelegramLongPollingBot {
     private void executeMessages(List<Validable> sendMessages) {
         for (Validable sendMessage : sendMessages) {
             try {
-                if (sendMessage == null) {
-                    return;
-                }
                 switch (sendMessage) {
                     case SendMessage sendmessage -> execute(sendmessage);
                     case SendSticker sendsticker -> execute(sendsticker);
