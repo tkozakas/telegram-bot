c089f6863fea0b167aa8d23f0abccda189a7778b
tomas6446 <tomas.kozakas@gmail.com>
2024-01-25 17:49:05 +0200

    Make stats sorted

--- src/main/java/org/churk/telegrambot/decorator/StatsListDecorator.java
+++ src/main/java/org/churk/telegrambot/decorator/StatsListDecorator.java
@@ -2,9 +2,9 @@ package org.churk.telegrambot.decorator;
 
 import org.churk.telegrambot.model.Stat;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class StatsListDecorator {
     private final List<Stat> statList;
@@ -14,10 +14,10 @@ public class StatsListDecorator {
     }
 
     public String getFormattedStats(String statsTable, String header, String footer) {
-        return IntStream
-                .iterate(0, i -> i < statList.size() && i < 10, i -> i + 1)
-                .mapToObj(i -> statsTable.
-                        formatted(i + 1, statList.get(i).getFirstName(), statList.get(i).getScore()))
+        return statList.stream()
+                .sorted(Comparator.comparing(Stat::getScore).reversed())
+                .limit(10)
+                .map(stat -> statsTable.formatted(statList.indexOf(stat) + 1, stat.getFirstName(), stat.getScore()))
                 .collect(Collectors.joining("", header, footer));
     }
 }
