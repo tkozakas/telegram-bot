961e425edc527b50b5ad819c88a402ad27094bd4
tomas6446 <tomas.kozakas@gmail.com>
2024-01-26 23:50:46 +0200

    Add multiple pattern matching, fix
    Add multiple pattern matching, fix

--- src/main/java/org/churk/telegrambot/model/Command.java
+++ src/main/java/org/churk/telegrambot/model/Command.java
@@ -4,27 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public enum Command {
-    START(".*/start\\b.*", "Start the bot"),
-    HELP(".*/help\\b.*", "Get help"),
-    REGISTER(".*/%sreg\\b.*", "Register yourself as a %s"),
-    DAILY_MESSAGE(".*/%s\\b.*", "Get today's %s"),
-    STATS(".*/%sstats\\b.*", "Get stats (use %sstats [year] for specific year)"),
-    STATS_ALL(".*/%sall\\b.*", "Get all-time stats"),
-    STATS_USER(".*/%sme\\b.*", "Get personal stats"),
-    FACT(".*/fact\\b.*", "Random fact of the day"),
-    STICKER(".*/sticker\\b.*", "Random sticker from a %s sticker set"),
-    REDDIT(".*/reddit\\b.*", "Random reddit picture (use /reddit [year] for specific subreddit)");
+    START(List.of(".*/start\\b.*"), "Start the bot"),
+    HELP(List.of(".*/help\\b.*"), "Get help"),
+    REGISTER(List.of(".*/%sreg\\b.*"), "Register yourself as a %s"),
+    DAILY_MESSAGE(List.of(".*/%s\\b.*"), "Get today's %s"),
+    STATS(List.of(".*/%sstats\\b.*"), "Get stats (use %sstats [year] for specific year)"),
+    STATS_ALL(List.of(".*/%sall\\b.*"), "Get all-time stats"),
+    STATS_USER(List.of(".*/%sme\\b.*"), "Get personal stats"),
+    FACT(List.of(".*/fact\\b.*"), "Random fact of the day"),
+    STICKER(List.of(".*/sticker\\b.*"), "Random sticker from a %s sticker set"),
+    REDDIT(List.of(".*/reddit\\b.*", ".*/meme\\b.*"), "Random reddit picture (use /reddit [year] for specific subreddit)"),
+    REELS(List.of(".*/reels\\b.*", ".*/reel\\b.*"), "Random reels video (use /reels [link] for specific reels video)");
 
-    private final String pattern;
+    private final List<String> patterns;
     private final String description;
 
     public static Command getTextCommand(String text, String botName) {
         return Arrays.stream(Command.values())
-                .filter(command -> text.matches(command.getPattern().formatted(botName)))
+                .filter(command -> command.getPatterns().stream()
+                        .anyMatch(pattern -> text.matches(pattern.formatted(botName))))
                 .findFirst()
                 .orElse(null);
     }
