ff95e3c352cb726846df679b70a0984cb059c5a2
tomas6446 <tomas.kozakas@gmail.com>
2024-01-28 15:00:27 +0200

    Fix package error

--- src/main/java/org/churk/telegrambot/handler/CommandHandler.java
+++ src/main/java/org/churk/telegrambot/handler/CommandHandler.java
@@ -1,13 +1,12 @@
 package org.churk.telegrambot.handler;
 
-import org.churk.telegrambot.exception.BotException;
 import org.churk.telegrambot.model.Command;
 import org.telegram.telegrambots.meta.api.interfaces.Validable;
 
 import java.util.List;
 
 public interface CommandHandler {
-    List<Validable> handle(HandlerContext context) throws BotException;
+    List<Validable> handle(HandlerContext context);
 
     Command getSupportedCommand();
 }
