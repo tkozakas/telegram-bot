e1ffee2db5e46203d41883b151d42fc6a3bc87e3
tomas6446 <tomas.kozakas@gmail.com>
2024-02-01 14:38:31 +0200

    Add removed builder

--- src/main/java/org/churk/telegrambot/handler/HandlerContext.java
+++ src/main/java/org/churk/telegrambot/handler/HandlerContext.java
@@ -1,12 +1,14 @@
 package org.churk.telegrambot.handler;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.List;
 
 @Data
+@Builder
 @AllArgsConstructor
 public final class HandlerContext {
     private Update update;
