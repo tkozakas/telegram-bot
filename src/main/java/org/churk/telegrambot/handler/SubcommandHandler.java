823cf5c8a036787d1dc2f5934949e207e2ee2765
tomas6446 <tomas.kozakas@gmail.com>
2024-03-03 22:59:49 +0200

    Remove unused interface

--- src/main/java/org/churk/telegrambot/handler/SubcommandHandler.java
+++
@@ -1,9 +0,0 @@
-package org.churk.telegrambot.handler;
-
-import org.churk.telegrambot.utility.HandlerContext;
-
-import java.util.List;
-
-public interface SubcommandHandler {
-    void handleSubcommand(HandlerContext context, List<String> args);
-}
