7161a1ed529d92f8f7b435e74a36025a07369f59
tomas6446 <tomas.kozakas@gmail.com>
2024-06-09 19:53:40 +0300

    Sort stats DESC

--- src/main/java/org/churk/telegrambot/repository/StatsRepository.java
+++ src/main/java/org/churk/telegrambot/repository/StatsRepository.java
@@ -12,19 +12,19 @@ import java.util.UUID;
 
 @Repository
 public interface StatsRepository extends JpaRepository<Stat, UUID> {
-    @Query("SELECT s FROM Stat s WHERE s.chatId = ?1 AND s.userId = ?2")
+    @Query("SELECT s FROM Stat s WHERE s.chatId = ?1 AND s.userId = ?2 ORDER BY s.score DESC")
     List<Stat> getStatsByChatIdAndUserId(Long chatId, Long userId);
 
-    @Query("SELECT s FROM Stat s WHERE s.chatId = ?1 AND s.year = ?2")
+    @Query("SELECT s FROM Stat s WHERE s.chatId = ?1 AND s.year = ?2 ORDER BY s.score DESC")
     List<Stat> getStatsByChatIdAndYear(Long chatId, int year);
 
-    @Query("SELECT s FROM Stat s WHERE s.chatId = ?1")
+    @Query("SELECT s FROM Stat s WHERE s.chatId = ?1 ORDER BY s.score DESC")
     List<Stat> findAllByChatId(Long chatId);
 
-    @Query("SELECT s FROM Stat s WHERE s.chatId = ?1 AND s.year = ?2")
+    @Query("SELECT s FROM Stat s WHERE s.chatId = ?1 AND s.year = ?2 ORDER BY s.score DESC")
     List<Stat> findAllByChatIdAndYear(Long chatId, int year);
 
-    @Query("SELECT s FROM Stat s WHERE s.chatId = ?1 AND s.firstName = ?2")
+    @Query("SELECT s FROM Stat s WHERE s.chatId = ?1 AND s.firstName = ?2 ORDER BY s.score DESC")
     List<Stat> getUserIdByChatIdAndFirstName(Long chatId, String firstName);
 
     @Modifying
