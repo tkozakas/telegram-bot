145346b556daaad083ea348f6602f58e27f24a58
tomas6446 <tomas.kozakas@gmail.com>
2024-06-16 14:03:15 +0300

    Correct initial prompt

--- src/main/java/org/churk/telegrambot/service/GroqService.java
+++ src/main/java/org/churk/telegrambot/service/GroqService.java
@@ -23,7 +23,7 @@ public class GroqService {
         if (!messageHistory.isEmpty()) {
             messages.addFirst(new GroqRequest.Message("system",
                     String.format("Please respond formally and avoid scientific or philosophical topics. Please focus on the conversation before you not the past." +
-                                    "But our conversation's summary so far: \"%s\". And this is the latest reply from you \"%s\"",
+                                    "But our conversation's summary so far: \"%s\". And this is the latest reply from you \"%s\". Dont tell us about our past conversation. Just focus on the current one.",
                             messageHistory, latestReply)));
         }
 
